d088d2367396bf98d0358036fd754ba7aba06cde
sborzic <sborzic@gmail.com>
2020-05-06 20:45:22 +0200

    added buttons(DOM manipilation exercise)

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,20 +1,37 @@
+let buttons = Array.from(document.getElementsByTagName('button'));
+
+buttons.forEach(button=>{
+  button.addEventListener('click', playerPlay);
+});
+
+let playerScore = 0;
+let compScore = 0;
+
+
 function computerPlay(){
   let rndNum = Math.floor(Math.random()*3)
   let options = ['rock', 'paper', 'scissors']
+  console.log(options[rndNum])
   return options[rndNum];
 }
 
-function playerPlay(){
-  let play = prompt('Rock, paper or scissors?')
-  return play.toLowerCase();
+function playerPlay(event){
+  let playerSelection = event.target.id
+  console.log(playerSelection);
+  let compSelection = computerPlay();
+  game(playerSelection, compSelection); 
+    
 }
 
-function game(){
-  let playerScore = 0;
-  let compScore = 0;
-  for(i=1; i<=5; i++){
-    let compSelection = computerPlay();
-    let playerSelection = playerPlay();
+function updateScore(playerScore, compScore){  
+  let playerBoard = document.querySelector('#player-score');
+  let computerBoard = document.querySelector('#computer-score');
+  playerBoard.textContent = `Player score ${playerScore}`;
+  computerBoard.textContent = `Computer score ${compScore}`;
+}
+
+
+function game(playerSelection, compSelection){
     if (compSelection == 'rock' && playerSelection =='scissors') {
       compScore+=1;
     } else if (compSelection == 'rock' && playerSelection == 'paper'){
@@ -29,5 +46,21 @@ function game(){
       compScore+=1;
     } else console.log('draw');
     console.log(`Player score: ${playerScore}, computer score: ${compScore}`);
+    updateScore(playerScore, compScore);
+    declareWinner()
+}
+
+function declareWinner(){
+  if (playerScore == 5) {
+    alert('Player wins');
+    buttons.forEach(button=>{
+      button.removeEventListener('click', playerPlay);
+    });
+  } else if (compScore == 5){
+    alert('Computer wins');
+    buttons.forEach(button=>{
+      button.removeEventListener('click', playerPlay);
+    });
   }
+
 }
